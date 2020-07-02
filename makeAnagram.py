5ad0b46dbd8e2dd5375df3d6bf86ab832df91ac2
Maykel Contreras Camacho <jotasenator@gmail.com>
2020-07-02 13:00:53 -0400

    Add files via upload
    In here the code that works for solving https://www.hackerrank.com/challenges/ctci-making-anagrams/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=strings

---
+++ makeAnagram.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+os.environ['OUTPUT_PATH'] = 'exercice.txt'
+
+# Complete the makeAnagram function below.
+def makeAnagram(a, b):
+
+    dic1 = {}
+
+    dic2 = {}
+
+    for i in a:
+
+        dic1[i]=a.count(i)
+
+    for j in b:
+
+        dic2[j]=b.count(j)
+
+
+    #letras que estan en las dos palabras pero que pueden tener distinta frecuencia
+    a_b_sobran = sum([abs(dic1[i]-dic2[i]) for i in dic1.keys() if i in dic2.keys()])
+
+    #letras que estan en una palabra pero que no estan en la otra
+    sobra_a = len([i for i in a if i not in b])
+    sobra_b = len([i for i in b if i not in a])
+
+    resultado_final_sobran = a_b_sobran + sobra_a + sobra_b
+
+    print(resultado_final_sobran)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = input()
+
+    b = input()
+
+    res = makeAnagram(a, b)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
